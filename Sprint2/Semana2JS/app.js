dd32ff03e07889186e12c3d0e68ca52747a53182
Fran <llanquinf@gmail.com>
2026-08-27 23:32:03 -0300

    terminado el tp para ya arrancar con la pagina grupal

--- Sprint2/Semana2JS/app.js
+++ Sprint2/Semana2JS/app.js
@@ -1,3 +1,5 @@
+// Ejercicio 1: Catálogo de Productos Dinámico
+
 const catalogoMuebles = [
     {
         id: 1,
@@ -24,33 +26,36 @@ const contenedor = document.querySelector('#product-container');
 catalogoMuebles.forEach(mueble => {
     const nuevoDiv = document.createElement('div')
     nuevoDiv.classList.add('product-card');
-    
+
     const nuevoH3 = document.createElement('h3');
     nuevoH3.textContent = mueble.nombre;
-    
+
     const nuevoP = document.createElement('p');
     nuevoP.textContent = mueble.precio;
-    
+
     const nuevoIMG = document.createElement('img');
     nuevoIMG.src = mueble.imagenURL;
-    
+
     nuevoDiv.appendChild(nuevoH3);;
     nuevoDiv.appendChild(nuevoP);;
     nuevoDiv.appendChild(nuevoIMG);;
-    
+
     contenedor.appendChild(nuevoDiv);
-    
 });
 
+// Ejercicio 2: Botón de "Añadir a Favoritos"
+
 const botonFavorito = document.querySelector('button');
-botonFavorito.addEventListener('click', function() {
+botonFavorito.addEventListener('click', function () {
     botonFavorito.classList.toggle('favorito');
-    if(botonFavorito.className === 'favorito') {
+    if (botonFavorito.className === 'favorito') {
         botonFavorito.textContent = '⭐ Quitar de Favoritos';
     } else {
         botonFavorito.textContent = '❤️ Añadir a Favoritos';
     }
 });
+
+// Ejercicio 3: Guardar y Cargar Preferencias con JSON
 
 const preferenciasUsuario = {
     tema: 'oscuro',
@@ -59,15 +64,94 @@ const preferenciasUsuario = {
 };
 
 const botonGuardar = document.querySelector('#btn-guardar');
-botonGuardar.addEventListener('click', function() {
+botonGuardar.addEventListener('click', function () {
     let stringJSON = JSON.stringify(preferenciasUsuario);
     localStorage.setItem('misPreferencias', stringJSON);
     alert('Preferencias guardadas');
 });
 
 const botonCargar = document.querySelector('#btn-cargar');
-botonCargar.addEventListener('click', function() {
+botonCargar.addEventListener('click', function () {
     stringJSON = localStorage.getItem('misPreferencias');
     let stringObjeto = JSON.parse(stringJSON);
     console.log('Objeto recuperado: ', stringObjeto);
 });
+
+// Ejercicio 4: Buscador de Usuarios de una API
+
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const nombreInput = document.querySelector('input').value;
+    buscarUsuario(nombreInput);
+});
+
+async function buscarUsuario(stringInput) {
+    try {
+        const url = `https://jsonplaceholder.typicode.com/users?username=${stringInput}`
+        const respuesta = await fetch(url);
+        if (!respuesta.ok) {
+            throw new Error(`Error HTTP: El estado es ${respuesta.status}`);
+        }
+        const datosUsuario = await respuesta.json();
+        const contenedorUsuario = document.querySelector('#contenedor-usuario');
+        
+        // Vaciamos el contenedor antes de procesar los resultados
+        contenedorUsuario.innerHTML = '';
+        
+        if (datosUsuario.length > 0) {
+            const usuario = datosUsuario[0];
+            
+            let nuevoP = document.createElement('p');
+            nuevoP.textContent = `Nombre: ${usuario.name}`;
+            contenedorUsuario.appendChild(nuevoP);
+            
+            nuevoP = document.createElement('p');
+            nuevoP.textContent = `Email: ${usuario.email}`;
+            contenedorUsuario.appendChild(nuevoP);
+            
+            nuevoP = document.createElement('p');
+            nuevoP.textContent = `Ciudad: ${usuario.address.city}`;
+            contenedorUsuario.appendChild(nuevoP);
+            
+        } else {
+            const mensajeError = document.createElement('p');
+            mensajeError.textContent = 'Usuario no encontrado';
+            contenedorUsuario.appendChild(mensajeError);
+        }
+    } catch (error) {
+        console.error("No se pudo obtener el usuario: ", error);
+    }
+}
+
+// Ejercicio 5: Galería de Fotos Asíncrona con Eventos
+
+crearGaleria();
+
+async function crearGaleria() {
+    try {
+        const url = "https://jsonplaceholder.typicode.com/photos?_limit=12";
+        const respuesta = await fetch(url);
+        if (!respuesta.ok) {
+            throw new Error(`Error HTTP: El estado es ${respuesta.status}`);
+        }
+        
+        const arrayFotos = await respuesta.json();
+        const contenedorGaleria = document.querySelector('#contenedor-galeria');
+        
+        arrayFotos.forEach(foto => {
+            const img = document.createElement('img'); 
+            img.src = foto.thumbnailUrl;
+            //img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfW1Fb06pCmx515uBcKB0f4YGFNuLtydfkrqeU-6KE9w&s=10';
+            img.alt = foto.title;
+            
+            img.addEventListener('click', function() {
+                alert(foto.title);
+            });
+            
+            contenedorGaleria.appendChild(img);
+        });
+    } catch (error) {
+        console.error("No se pudo obtener el usuario: ", error);
+    }
+}
