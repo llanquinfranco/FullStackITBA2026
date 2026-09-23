52b4224ffb448e08a70951a0a8b80696849d8e08
Fran <llanquinf@gmail.com>
2026-09-22 22:37:54 -0300

    react con viteeeee

---
+++ Sprint 4 - Desarrollo Frontend con React/Semana 1 - React y JSX/client/src/components/TarjetaPresentacion.jsx
@@ -0,0 +1,11 @@
+function TarjetaPresentacion() {
+    return (
+        <div>
+            <h2>Ada Lovelace</h2>
+            <p>Programadora y Matematica</p>
+            <p>Considerada la primera programadora de la historia</p>
+        </div>
+    );
+}
+
+export default TarjetaPresentacion;
